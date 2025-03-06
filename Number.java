26f27be88a8303c994d3236eb4d8aa93ed5e0f78
ArianaAdame <132471158+ArianaAdame@users.noreply.github.com>
2025-03-05 17:55:51 -0800

    Update Number.java

--- Number.java
+++ Number.java
@@ -1,35 +1,42 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
+package hashtableintlab;
 
-//Name -
-
-public class Number
-{
+public class Number {
 	private int theValue;
 	
-	public Number(int value)
-	{
-
-
+	/* Parameterized constructor
+	 * @param value			value for theValue*/
+	public Number(int value) {
+		theValue = value;
 	}	
 	
-	public int getValue()
-	{
+	/* Returns theValue
+	 * @return int			theValue */
+	public int getValue() {
 		return theValue;
 	}
 	
-	public boolean equals(Object obj)
-	{
-		return false;
+	/* Checks if two number objects are equal
+	 * @return true			objects equal
+	 * @return false		objects not equal */
+	public boolean equals(Number obj) {
+		if (theValue == obj.getValue()) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	} 
 	
-	public int hashCode()
-	{
-		return 0;
+	/* Returns the generated hashCode value
+	 * @return int			hashCode */
+	public int hashCode() {
+		return theValue%10;
 	}
 
-	public String toString()
-	{
-		return "";
-	}	
+	/* Prints information about the Number object
+	 * @return String		theValue */
+	public String toString() {
+		return "" + theValue;
+	}
+	
 }
